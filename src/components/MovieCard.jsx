4a63b4e3e1a8644263a28610772fcf82ecff5b10
wlsdud0448 <wlsdud0448@naver.com>
2024-06-03 17:16:37 +0900

    mission1

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,19 +1,18 @@
 import React from 'react';
-import { useHistory } from 'react-router-dom';
+import './MovieCard.css';
+import { useNavigate } from 'react-router-dom';
 
 const MovieCard = ({ movie }) => {
-    const history = useHistory();
-
+    const navigate = useNavigate();
     const handleClick = (e) => {
         e.preventDefault();
-        history.push('/detail');
+
+        navigate('/detail');
     };
-
-    const movieImageUrl = movie.backdrop_path ? `https://image.tmdb.org/t/p/w500${movie.backdrop_path}` : '';
-
+    console.log(movie);
     return (
         <li key={movie.id} onClick={handleClick} className="card-box">
-            {movieImageUrl && <img src={movieImageUrl} alt={movie.title} loading="lazy" />} {/* 이미지 URL이 존재할 때만 이미지를 렌더링 */}
+            <img src={`https://image.tmdb.org/t/p/w500${movie.backdrop_path}`} alt={movie.title} />
             <h3>{movie.title}</h3>
             <p>⭐️ {movie.vote_average}</p>
         </li>
