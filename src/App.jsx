ec66cae0834095b4bdcfc9d5196aa341b5972fa4
wlsdud0448 <wlsdud0448@naver.com>
2024-06-11 18:01:42 +0900

    mission5

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,35 @@
-import { Outlet } from 'react-router-dom';
-import NavBar from './components/Navbar';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Navbar from './components/Navbar';
 import MovieBanner from './components/MovieBanner';
 import FooterPage from './components/FooterPage';
+import MovieList from './components/MovieList';
+import MovieDetail from './components/MovieDetail';
+import SignupPage from './components/SignupPage';
+import LoginPage from './components/LoginPage';
+import SearchPage from './components/SearchPage';
 
 function App() {
     return (
-        <div className="App">
-            <header>
-                <NavBar />
-            </header>
-            <main>
-                <MovieBanner />
-                <Outlet />
-                <FooterPage />
-            </main>
-        </div>
+        <BrowserRouter>
+            <div className="App">
+                <header>
+                    <Navbar />
+                </header>
+                <main>
+                    <MovieBanner />
+                    <Routes>
+                        <Route path="/" element={<MovieList />} />
+                        <Route path="/movie/:id" element={<MovieDetail />} />
+                        <Route path="/signup" element={<SignupPage />} />
+                        <Route path="/login" element={<LoginPage />} />
+                        <Route path="/search" element={<SearchPage />} />
+                    </Routes>
+                </main>
+                <footer>
+                    <FooterPage />
+                </footer>
+            </div>
+        </BrowserRouter>
     );
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,26 +1,9 @@
-import App from './App.jsx';
+import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import MovieDetail from './components/MovieDetail.jsx';
-import MovieList from './components/MovieList.jsx';
-import SignupPage from './components/SignupPage.jsx';
-import LoginPage from './components/LoginPage.jsx';
-import SearchPage from './components/SearchPage.jsx';
-import MovieBanner from './components/MovieBanner.jsx';
+import App from './App';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <BrowserRouter>
-        <Routes>
-            <Route path="/" element={<App />}>
-                <Route index element={<MovieList />} />
-                <Route path="/movie/:id" element={<MovieDetail />} />
-                <Route path="/signup" element={<SignupPage />} />
-                <Route path="/login" element={<LoginPage />} />
-                <Route path="/search" element={<SearchPage />} />
-                <Route path="/banner" element={<MovieBanner />} />
-            </Route>
-        </Routes>
-    </BrowserRouter>
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
 );
-
-export default App;
